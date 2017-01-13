c8d2f384f00fe60cbebd06374fd4700c145b9ed9
Adam Eilers <adam.eilers@gmail.com>
2017-01-13 13:43:47 -0800

    adding correct name for base router class

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ exports.Aggregate = require('./lib/aggregate');
 exports.Command = require('./lib/command');
 exports.Event = require('./lib/event');
 exports.Handler = require('./lib/handler');
-exports.Router = require('./lib/router');
+exports.MessageRouter = require('./lib/router');
 exports.EventStore = require('./lib/store');
