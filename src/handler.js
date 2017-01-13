72520708613c1672caa28c7c3413ab2008f08fcb
Adam Eilers <adam.eilers@gmail.com>
2017-01-13 15:01:21 -0800

    updating 'handler' to include reference to desired 'repository' when instantiated

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,9 @@
 export default class Handler {
 
+  constructor(repository) {
+    this.repository = repository;
+  }
+
   execute = async () => {
     throw new Error('you need to set an execute method for your command handler');
   }
