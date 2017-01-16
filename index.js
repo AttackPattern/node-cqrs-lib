8a8367badcfe9c6037c0db44e5cf2b5c055dff44
Adam Eilers <adam.eilers@gmail.com>
2017-01-16 08:49:29 -0800

    removing specific project dependencies and moving router/store to producer repo

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ exports.Aggregate = require('./lib/aggregate');
 exports.Command = require('./lib/command');
 exports.Event = require('./lib/event');
 exports.Handler = require('./lib/handler');
-exports.MessageRouter = require('./lib/router');
-exports.EventStore = require('./lib/store');
+exports.Message = require('./lib/message');
