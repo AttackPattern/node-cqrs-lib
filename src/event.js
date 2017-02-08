b66f0403b4236b8e5af41060abfa26707c37e68a
Adam Eilers <adam.eilers@gmail.com>
2017-02-08 15:11:19 -0800

    tweaking Event constructor to add spread functionality to base class

--- src/event.js
+++ src/event.js
@@ -3,8 +3,8 @@ import Message from './message';
 export default class Event extends Message {
   type = 'event';
 
-  constructor(data) {
-    super(data);
+  constructor(id, data) {
+    super({ ...data, id });
 
     this.timestamp = new Date().toISOString();
   }
