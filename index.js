c4e2241788ba8c389a7b1e5078cf2bd27b59117b
Brett Morien <bmorien@gmail.com>
2017-06-30 11:07:44 -0700

    Remove ioc container after moving to its own repo.

--- index.js
+++ index.js
@@ -8,7 +8,4 @@ exports.AggregateCommandHandler = require('./lib/aggregateCommandHandler');
 exports.Repository = require('./lib/repository');
 exports.ValidationError = require('./lib/validationError');
 exports.AuthorizationError = require('./lib/authorizationError');
-
 exports.CommandScheduling = require('./lib/commandScheduling');
-exports.Container = require('./lib/container').default;
-exports.inject = require('./lib/container').inject;
